56d33be9bb7c69b700ac060c09f68b75bca30178
Dani <danikuperden@gmail.com>
2022-12-12 18:37:52 +0100

    daniii

--- app/src/main/java/com/cristobalbernal/trabajoenequipo/MainActivity.java
+++ app/src/main/java/com/cristobalbernal/trabajoenequipo/MainActivity.java
@@ -10,9 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hola maricon
-        System.out.println("Adios");
-        System.out.println("adiosssss");
-        System.out.println("yombi tonto");
     }
 }
